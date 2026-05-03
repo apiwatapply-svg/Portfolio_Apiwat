c6a9a1e747424c87fa3618115f34aee75fe74c3a
apiwatapply-svg <apiwat.apply@gmail.com>
2026-05-03 09:19:46 +0700

    ui(modal): adjust vertical alignment to items-end

--- components/ProjectModal.tsx
+++ components/ProjectModal.tsx
@@ -54,7 +54,7 @@ export default function ProjectModal({ project, onClose }: Props) {
 
   return (
     <AnimatePresence>
-      <div className="fixed inset-0 z-[100] flex items-center justify-center p-4 sm:p-6">
+      <div className="fixed inset-0 z-[100] flex items-end justify-center p-4 sm:p-6">
         {/* Backdrop */}
         <motion.div
           initial={{ opacity: 0 }}
